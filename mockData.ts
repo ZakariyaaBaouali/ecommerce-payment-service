90839c66d6a3d6fc459fe803e87389815d833a57
zakaria <zakariabaouali@outlook.com>
2024-06-21 00:06:42 +0100

    create mock product data to test

---
+++ mockData.ts
@@ -0,0 +1,25 @@
+import { IProduct } from "./src/dto";
+
+export const productDB: IProduct[] = [
+  {
+    id: 1,
+    name: "product 1",
+    description: "Gut product for mich",
+    price: 15.5,
+    avatar: "",
+  },
+  {
+    id: 2,
+    name: "product 2",
+    description: "Gut product for dich",
+    price: 10,
+    avatar: "",
+  },
+  {
+    id: 3,
+    name: "product 2",
+    description: "Gut product for ihn",
+    price: 12.99,
+    avatar: "",
+  },
+];
