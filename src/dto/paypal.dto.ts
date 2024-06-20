6f132b7c6f9cf0e18ac22adec661a5bbdf5568f6
zakaria <zakariabaouali@outlook.com>
2024-06-21 00:29:11 +0100

    add paypal dto for type safety

---
+++ src/dto/paypal.dto.ts
@@ -0,0 +1,92 @@
+export interface PaypalToken {
+  access_token: string;
+  expires_in: number;
+}
+
+export interface PaypalOrderItem {
+  name: string;
+  description: string;
+  quantity: string;
+  unit_amount: {
+    currency_code: string;
+    value: string;
+  };
+}
+
+//the schema must be like this to ge no errors
+export interface PaypalIntent {
+  intent: string;
+  purchase_units: [
+    {
+      items: PaypalOrderItem[];
+      amount: {
+        currency_code: string;
+        value: string;
+        breakdown: {
+          item_total: {
+            currency_code: string;
+            value: string;
+          };
+        };
+      };
+    }
+  ];
+  application_context: {
+    return_url: string;
+    cancel_url: string;
+    user_action?: string;
+  };
+}
+
+//schema to get pay link
+export interface PaypalPayLinks {
+  links: [
+    {
+      href: string;
+      rel: string;
+    }
+  ];
+}
+
+//
+export interface PaypalChckoutResult {
+  status: string;
+  payment_source: {
+    paypal: {
+      email_address: string;
+      name: {
+        given_name: string;
+        surname: string;
+      };
+    };
+  };
+  purchase_units: {
+    shipping: {
+      address: {
+        address_line_1: string;
+        admin_area_1: string;
+        admin_area_2: string;
+        postal_code: string;
+        country_code: string;
+      };
+    };
+    payments: {
+      captures: {
+        eller_receivable_breakdown: {
+          gross_amount: {
+            currency_code: string;
+            value: string;
+          };
+          paypal_fee: {
+            currency_code: string;
+            value: string;
+          };
+          net_amount: {
+            currency_code: string;
+            value: string;
+          };
+        };
+      };
+    };
+  };
+}
