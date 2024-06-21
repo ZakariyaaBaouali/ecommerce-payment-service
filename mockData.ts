69ce9a5fa332bb3a9dc34918649e0b1ee4321a40
zakaria <zakariabaouali@outlook.com>
2024-06-21 18:28:21 +0100

    add avatar to mock products

--- mockData.ts
+++ mockData.ts
@@ -6,20 +6,23 @@ export const productDB: IProduct[] = [
     name: "product 1",
     description: "Gut product for mich",
     price: 15.5,
-    avatar: "",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSxf5ha2-HCoZC08jbZrh74U_FlUctCpjsg&s",
   },
   {
     id: 2,
     name: "product 2",
     description: "Gut product for dich",
     price: 10,
-    avatar: "",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSxf5ha2-HCoZC08jbZrh74U_FlUctCpjsg&s",
   },
   {
     id: 3,
-    name: "product 2",
+    name: "product 3",
     description: "Gut product for ihn",
     price: 12.99,
-    avatar: "",
+    avatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNSxf5ha2-HCoZC08jbZrh74U_FlUctCpjsg&s",
   },
 ];
